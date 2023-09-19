8ae4c0aaaba4fa2f24baa9703be12f2054d1a409
LauraWaen <laurawaenerberg@gmail.com>
2023-09-19 17:13:48 +0300

    työtunnit yhteensä kentän lisäys

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,32 @@ import Button from '@mui/material/Button';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import 'dayjs/locale/fi';
 
-
-
 function App() {
-
   const locales = ['fi'];
   const [startTime, setStartTime] = React.useState(dayjs(''));
   const [endTime, setEndTime] = React.useState(dayjs(''));
+  const [sumInMinutes, setSumInMinutes] = React.useState(0);
+  const [sumInHours, setSumInHours] = React.useState(0);
+
+  // Laske summa minuutteina ja tunteina
+  const calculateSum = () => {
+    const startTimeMs = startTime.valueOf();
+    const endTimeMs = endTime.valueOf();
+
+    if (!isNaN(startTimeMs) && !isNaN(endTimeMs)) {
+      const sumInMs = endTimeMs - startTimeMs;
+      setSumInMinutes(sumInMs);
+
+      // Laske summa tunteina
+      const sumInHrs = sumInMs / (1000 * 60 * 60);
+      setSumInHours(sumInHrs);
+    }
+  };
 
   return (
     <>
       <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale='fi'>
-      <DateTimePicker
+        <DateTimePicker
           label="Aloitusaika"
           viewRenderers={{
             hours: null,
@@ -38,11 +52,13 @@ function App() {
           onChange={(newValue) => setEndTime(newValue)}
         />
       </LocalizationProvider>
-      <Button variant="contained" size="large" onClick={() => {
-    alert(endTime);
-  }}>Lähetä</Button>
+      
+      <Button variant="contained" size="large" onClick={calculateSum}>Laske</Button>
+      
+    
+      <p className="tyoaika">Työaika yhteensä: {sumInHours} tuntia</p>
     </>
   )
 }
 
-export default App
+export default App;
