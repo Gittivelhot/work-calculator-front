9f01a6bf856ea07820aaa9783c80ae4d1ed19b4c
attemkinen <atte.makinen1@outlook.com>
2023-09-25 20:53:20 +0300

    added hourlyRate functionality and snackbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import * as React from 'react';
 import dayjs from 'dayjs';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { TextField } from '@mui/material';
 import Button from '@mui/material/Button';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import 'dayjs/locale/fi';
+import Snackbar from '@mui/material/Snackbar';
+import { useState } from 'react';
 
 function App() {
   const locales = ['fi'];
@@ -12,21 +15,42 @@ function App() {
   const [endTime, setEndTime] = React.useState(dayjs(''));
   const [sumInMinutes, setSumInMinutes] = React.useState(0);
   const [sumInHours, setSumInHours] = React.useState(0);
+  const [hourlyRate, setHourlyRate] = React.useState(0);
+  const [earnings, setEarnings] = React.useState(0);
+  const [open, setOpen] = useState(false);
+  const [msg, setMsg] = useState([]);
 
   // Laske summa minuutteina ja tunteina
   const calculateSum = () => {
     const startTimeMs = startTime.valueOf();
     const endTimeMs = endTime.valueOf();
-
+  
     if (!isNaN(startTimeMs) && !isNaN(endTimeMs)) {
-      const sumInMs = endTimeMs - startTimeMs;
-      setSumInMinutes(sumInMs);
-
-      // Laske summa tunteina
-      const sumInHrs = sumInMs / (1000 * 60 * 60);
-      setSumInHours(sumInHrs);
+      if (startTimeMs < endTimeMs) {
+        const sumInMs = endTimeMs - startTimeMs;
+        const sumInHrs = sumInMs / (1000 * 60 * 60);
+        const sumInMin = sumInMs / (1000 * 60);
+  
+        const hourlyRateValue = parseFloat(hourlyRate); // Muunna tuntipalkka numeroksi
+        if (!isNaN(hourlyRateValue)) {
+          const earnings = hourlyRateValue * sumInHrs;
+          setSumInHours(sumInHrs.toFixed(2)); // Pyöristetään 2 desimaaliin
+          setSumInMinutes(sumInMin.toFixed(2));
+          setEarnings(earnings.toFixed(2)); // Näytä ansaitut tulot
+        } else {
+          setMsg("Tarkista tuntipalkka. Syötä kelvollinen numero.");
+          setOpen(true);
+        }
+      } else {
+        setMsg("Aloitusaika tulee olla ennen lopetusaikaa.");
+          setOpen(true);
+      }
+    } else {
+      setMsg("Aseta aloitus- ja lopetusaika ennen laskemista.");
+          setOpen(true);
     }
   };
+  
 
   return (
     <>
@@ -52,11 +76,26 @@ function App() {
           onChange={(newValue) => setEndTime(newValue)}
         />
       </LocalizationProvider>
-      
-      <Button variant="contained" size="large" onClick={calculateSum}>Laske</Button>
+      <TextField
+        label="Tuntipalkka"
+        type="number"
+        value={hourlyRate}
+        onChange={(e) => setHourlyRate(e.target.value)}
+        InputProps={{ inputProps: { min: 0 } }} // Varmista, että syötetty arvo on ei-negatiivinen
+        
+      />
+      <Button variant="contained" size="medium" onClick={calculateSum}>Laske</Button>
       
     
       <p className="tyoaika">Työaika yhteensä: {sumInHours} tuntia</p>
+      <p className="ansiot">Ansaitut tulot: {earnings} euroa</p>
+
+      <Snackbar
+        open={open}
+        autoHideDuration={3500}
+        onClose={() => setOpen(false)}
+        message={msg}
+      />
     </>
   )
 }
