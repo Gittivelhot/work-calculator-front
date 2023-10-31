93e794777490055f24007816079085fd9263387a
LauraWaen <laurawaenerberg@gmail.com>
2023-10-31 13:05:55 +0200

    dropdown menu

--- src/components/ButtonAppBar.jsx
+++ src/components/ButtonAppBar.jsx
@@ -5,11 +5,24 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import React, { useState } from 'react';
+
 
 export default function ButtonAppBar() {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
-    <Box sx={{ flexGrow: 1}}>
+    <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" style={{ backgroundColor: '#262626' }}>
         <Toolbar>
           <IconButton
@@ -18,15 +31,30 @@ export default function ButtonAppBar() {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={handleMenuClick}
           >
             <MenuIcon />
           </IconButton>
+
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Työtunti laskuri
           </Typography>
-          <Button color="success" variant='contained'>Kirjaudu sisään</Button>
+
+          <Button color="success" variant="contained">Kirjaudu sisään</Button>
+
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+          >
+            <MenuItem onClick={handleMenuClose}>Kuukausinäkymä</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Jotain</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Jotain</MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
     </Box>
   );
 }
+
+
