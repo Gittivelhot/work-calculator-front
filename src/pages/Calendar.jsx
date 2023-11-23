07ecd714e24c5fc076a9ccc050162a4a40e0c7bb
attemkinen <atte.makinen1@outlook.com>
2023-11-23 16:48:26 +0200

    changed calender events to clickable and made a pop-up windows when calender event is clicked

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -2,10 +2,16 @@ import React, { useEffect, useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import TextField from '@mui/material/TextField';
 
 
 export default function Calendar() {
   const [workingHours, setWorkingHours] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
 
   useEffect(() => {
     fetch('/api/userdetails', {
@@ -25,6 +31,11 @@ export default function Calendar() {
       .catch((err) => console.log(err))
   }, []);
 
+  const handleEventClick = (eventClickInfo) => {
+    setSelectedEvent(eventClickInfo.event);
+    setOpen(true);
+  };
+
   const events = workingHours.map(item => ({
     title: `Work - ${item.id}`,
     start: new Date(item.startTime),
@@ -36,9 +47,48 @@ export default function Calendar() {
     <div style={{ width: '94%', marginTop: '5%', paddingLeft: '5%', paddingRight: '5%' }}>
       <FullCalendar
         events={events}
+        eventClick={handleEventClick}
         eventContent={(event) => {
           return (
             <div>
+                  <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>Työaika yksityiskohdat</DialogTitle>
+        <DialogContent>
+          {selectedEvent && (
+            <>
+              <TextField
+                autoFocus
+                margin="dense"
+                name="title"
+                value={selectedEvent.title}
+                label="Title"
+                fullWidth
+                variant="standard"
+                disabled
+              />
+              <TextField
+                margin="dense"
+                name="start"
+                value={selectedEvent.start ? selectedEvent.start.toLocaleString() : ''}
+                label="Start Time"
+                fullWidth
+                variant="standard"
+                disabled
+              />
+              <TextField
+                margin="dense"
+                name="end"
+                value={selectedEvent.end ? selectedEvent.end.toLocaleString() : ''}
+                label="End Time"
+                fullWidth
+                variant="standard"
+                disabled
+              />
+              {/* Add other fields with event information */}
+            </>
+          )}
+        </DialogContent>
+      </Dialog>
               <p>{event.event._def.title}</p>
               {event.event.start && event.event.end && (
                 <p>
@@ -49,7 +99,7 @@ export default function Calendar() {
             </div>
           );
         }}
-        aspectRatio={1.5}
+        aspectRatio={2.0}
         plugins={[dayGridPlugin, timeGridPlugin]}
         initialView="timeGridWeek"
         headerToolbar={{
